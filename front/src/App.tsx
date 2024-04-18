fb2630cd94aebaca4eec77edd507411aad40cfbd
Arnaud Laforgue <arnaud.laforgue@gmail.com>
2024-04-18 16:34:58 +0200

    second commit

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,60 +1,93 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import './App.css'
-import { socket } from './modules/socket'
+//import { socket } from './modules/socket'
 
 function App() {
   const [numbercomparison, updateNumbercomparison] = useState<string>('')
   const [mynumber, updateMynumber] = useState<number>(0)
   const [isthreeusers, updateIsthreeusers] = useState<boolean>(false)
-  useEffect(() => {
-    socket.off('reponse')
-    socket.off('reponse_utilisateurs')
-    // socket.on('number message', (msg: string) => {
-    //   console.log('new message received', msg)
-    //   updateNumbercomparison(msg);
-    // })
-    socket.on('reponse_utilisateurs', (reponseBoolean: boolean) => {
-      updateIsthreeusers(reponseBoolean);
-    })
-    socket.on('reponse', (reponseString: string) => {
-      updateNumbercomparison(reponseString);
-    })
+  const [forme,setForme] = useState<boolean[][]>([[false,false,false],[false,false,false],[false,false,false]])
+  // useEffect(() => {
+  //   socket.off('reponse')
+  //   socket.off('reponse_utilisateurs')
+  //   // socket.on('number message', (msg: string) => {
+  //   //   console.log('new message received', msg)
+  //   //   updateNumbercomparison(msg);
+  //   // })
+  //   socket.on('reponse_utilisateurs', (reponseBoolean: boolean) => {
+  //     updateIsthreeusers(reponseBoolean);
+  //   })
+  //   socket.on('reponse', (reponseString: string) => {
+  //     updateNumbercomparison(reponseString);
+  //   })
 
 
-    return () => {
-      socket.off('reponse')
-      socket.off('reponse_utilisateurs')
-    }
-  })
+  //   return () => {
+  //     socket.off('reponse')
+  //     socket.off('reponse_utilisateurs')
+  //   }
+  // })
 
+  const handleChange = useCallback((i:number,j:number)=>{
+    let matrice = [...forme];
+    matrice[i][j]=true;
+    setForme(matrice);
+  },[forme])
   return (
     <div>
-      {isthreeusers ?
-      <div>
-        <div>{numbercomparison}</div>
-        {numbercomparison!=='Bravo'?
-          <form action="">
-            <input 
-              type="number"
-              id="message" 
-              onChange={(e) => updateMynumber(parseInt(e.target.value))}
-              value={mynumber} 
-            />
-            <button
-              onClick={(e) => {
-                e.preventDefault()
-                socket.emit('number message', mynumber)
-                updateMynumber(0)
-              }}
-            >
-              Send
-            </button>
-          </form>:
-        <div>Un joueur a gagné</div>
-        }
-      </div> :
-        <div>La partie commencera dès qu'il y aura trois utilisateurs</div>
-      }
+      <div className='cadre'>
+        <div className='line'>
+          <div className='box' onClick={() => handleChange(0,0)}>
+            {forme[0][0]?
+            <div className='symbol'>X</div>:
+            <div></div>}
+          </div>
+          <div className='box' onClick={() => handleChange(0,1)}>
+          {forme[0][1]?
+            <div className='symbol'>X</div>:
+            <div></div>}
+          </div>
+          <div className='box' onClick={() => handleChange(0,2)}>
+          {forme[0][2]?
+            <div className='symbol'>X</div>:
+            <div></div>}
+          </div>
+        </div>
+        <div className='line'>
+          <div className='box' onClick={() => handleChange(1,0)}>
+          {forme[1][0]?
+            <div className='symbol'>X</div>:
+            <div></div>}
+          </div>
+          <div className='box' onClick={() => handleChange(1,1)}>
+          {forme[1][1]?
+            <div className='symbol'>X</div>:
+            <div></div>}
+          </div>
+          <div className='box' onClick={() => handleChange(1,2)}>
+          {forme[1][2]?
+            <div className='symbol'>X</div>:
+            <div></div>}
+          </div>
+        </div>
+        <div className='line'>
+          <div className='box' onClick={() => handleChange(2,0)}>
+          {forme[2][0]?
+            <div className='symbol'>X</div>:
+            <div></div>}
+          </div>
+          <div className='box' onClick={() => handleChange(2,1)}>
+          {forme[2][1]?
+            <div className='symbol'>X</div>:
+            <div></div>}
+          </div>
+          <div className='box' onClick={() => handleChange(2,2)}>
+          {forme[2][2]?
+            <div className='symbol'>X</div>:
+            <div></div>}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
